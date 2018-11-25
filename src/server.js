e828a82d4784bc1b784467bb3a3bfd477dec232b
António Almeida <theantonioalmeida@gmail.com>
2018-11-25 23:25:03 +0000

    Create sample route and average sales value route

--- src/server.js
+++ src/server.js
@@ -1,8 +1,28 @@
 import jsonServer from 'json-server'
+import read from 'read-file'
 
 const server = jsonServer.create()
 const router = jsonServer.router('db.json')
 const middlewares = jsonServer.defaults()
+
+const db = router.db.__wrapped__;
+
+//Custom route test
+server.get('/echo', (req, res) => {
+  res.jsonp(req.query);
+})
+
+//Average sales value
+server.get('/sales/average-sales-value', (req, res) => {
+
+	const { NumberOfEntries, TotalCredit } = db.SourceDocuments.SalesInvoices;
+
+	const averageSalesValue = TotalCredit/NumberOfEntries;
+
+	res.jsonp({
+		value: averageSalesValue,
+	});
+});
 
 server.use(middlewares)
 server.use(router)
