8b87af878c286a76af17747ac3fe2cb48cd85a36
António Almeida <theantonioalmeida@gmail.com>
2018-11-29 14:26:46 +0000

    Add top selling products route

--- src/server.js
+++ src/server.js
@@ -27,8 +27,42 @@ server.get('/sales/average-sales-value', (req, res) => {
 	});
 });
 
+server.get('/sales/top-selling-products', (req, res) => {
+
+	let products = new Map();
+
+	let i = 0;
+	db.SalesInvoices.forEach((invoice) => {
+
+		const type = invoice.InvoiceType;
+
+		// Document type must be 'Fatura', 'Fatura Simplificada', 'Fatura Recibo' or 'Venda a Dinheiro'
+		if(invoice.Line.length && (type == 'FT' || type == 'FS' || type == 'VD'))
+			invoice.Line.forEach((line) => {
+				const { ProductDescription, Quantity } = line;
+
+				if(products.has(ProductDescription))
+					products.set(ProductDescription, products.get(ProductDescription) + parseInt(Quantity));
+				else
+					products.set(ProductDescription, parseInt(Quantity));
+			});
+	});
+
+	res.json(strMapToObj(products));
+});
+
 server.use(middlewares)
 server.use(router)
 server.listen(3000, () => {
   console.log('Server running at http://localhost:3000')
 })
+
+function strMapToObj(strMap) {
+    let obj = Object.create(null);
+    for (let [k,v] of strMap) {
+        // We don’t escape the key '__proto__'
+        // which can cause problems on older engines
+        obj[k] = v;
+    }
+    return obj;
+}
