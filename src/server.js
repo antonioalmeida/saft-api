e997f96c4342e44dfbb9591c348382038377e962
AndreFCruz <andrecruz97@gmail.com>
2018-11-30 17:02:57 +0000

    fixed sales routes

--- src/server.js
+++ src/server.js
@@ -49,11 +49,11 @@ server.get('/sales/top-selling-products', (req, res) => {
 	});
 
 	products = Object.keys(products)
-		  .sort((a,b) => products[b].Quantity - products[a].Quantity).map(elem => ({
-			  ProductCode: elem,
-			  ProductDescription: products[elem].ProductDescription,
-			  UnitPrice: products[elem].UnitPrice,
-			  Quantity: products[elem].Quantity
+			.sort((a,b) => products[b].Quantity - products[a].Quantity).map(elem => ({
+				ProductCode: elem,
+				ProductDescription: products[elem].ProductDescription,
+				UnitPrice: products[elem].UnitPrice,
+				Quantity: products[elem].Quantity
 			}));
 
 	res.json(products);
@@ -65,12 +65,16 @@ server.get('/sales/total-gross-sales', (req, res) => {
 
 	let totalSales = 0;
 	db.SalesInvoices.forEach((invoice) => {
+		const type = invoice.InvoiceType;
+		if(!(invoice.Line.length && (type == 'FT' || type == 'FS' || type == 'FR' || type == 'VD')))
+			return;
+
 		let invoiceDate = new Date(invoice.InvoiceDate);
 		if ((startDate == null || startDate <= invoiceDate) && (endDate == null || invoiceDate <= endDate))
 			totalSales += parseFloat(invoice.DocumentTotals.GrossTotal);
 	});
 
-	res.json({ totalSales: totalSales });
+	res.json({ totalGrossSales: totalSales });
 });
 
 server.get('/sales/total-net-sales', (req, res) => {
@@ -79,26 +83,16 @@ server.get('/sales/total-net-sales', (req, res) => {
 
 	let totalSales = 0;
 	db.SalesInvoices.forEach((invoice) => {
+		const type = invoice.InvoiceType;
+		if(!(invoice.Line.length && (type == 'FT' || type == 'FS' || type == 'FR' || type == 'VD')))
+			return;
+
 		let invoiceDate = new Date(invoice.InvoiceDate);
 		if ((startDate == null || startDate <= invoiceDate) && (endDate == null || invoiceDate <= endDate))
 			totalSales += parseFloat(invoice.DocumentTotals.NetTotal);
 	});
 
-	res.json({ totalSales: totalSales });
-});
-
-server.get('/sales/total-tax', (req, res) => {
-	let startDate = 'start-date' in req.query ? new Date(req.query['start-date']) : null;
-	let endDate = 'end-date' in req.query ? new Date(req.query['end-date']) : null;
-
-	let totalSales = 0;
-	db.SalesInvoices.forEach((invoice) => {
-		let invoiceDate = new Date(invoice.InvoiceDate);
-		if ((startDate == null || startDate <= invoiceDate) && (endDate == null || invoiceDate <= endDate))
-			totalSales += parseFloat(invoice.DocumentTotals.TaxPayable);
-	});
-
-	res.json({ totalSales: totalSales });
+	res.json({ totalNetSales: totalSales });
 });
 
 server.use(middlewares)
